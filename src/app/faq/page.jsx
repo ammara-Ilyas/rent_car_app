d4e1e5aefa8fd5123890ee0b4676b5fff4b5a396
ammara-Ilyas <ismarailyas545@gmail.com>
2025-03-06 15:17:32 +0500

    minor change

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,16 +1,24 @@
+// import React from "react";
+
+// import Hero from "@/components/miniWidgets/Hero";
+// import FAQAccordion from "@/components/miniWidgets/faqs";
+// import BlogSlider from "@/components/widgets/home/BlogSlider";
+// const page = () => {
+//   return (
+//     <>
+//       <Hero />
+//       <FAQAccordion />
+//       <BlogSlider />
+//     </>
+//   );
+// };
+
+// export default page;
+
 import React from "react";
 
-import Hero from "@/components/miniWidgets/Hero";
-import FAQAccordion from "@/components/miniWidgets/faqs";
-import BlogSlider from "@/components/widgets/home/BlogSlider";
-const page = () => {
-  return (
-    <>
-      <Hero />
-      <FAQAccordion />
-      <BlogSlider />
-    </>
-  );
-};
+function page() {
+  return <div>hello</div>;
+}
 
 export default page;
