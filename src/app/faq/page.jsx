efc5c512a2a069f7f3b5a234422fe160492ff5f8
ammara-Ilyas <ismarailyas545@gmail.com>
2025-03-08 23:20:45 +0500

    syapa faq removed

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,16 +1,24 @@
+// import React from "react";
+
+// import Hero from "@/components/miniWidgets/Hero";
+// import FAQAccordion from "@/components/miniWidgets/faqs";
+// import BlogSlider from "@/components/widgets/home/BlogSlider";
+// const page = () => {
+//   return (
+//     <>
+//       <Hero />
+//       <FAQAccordion />
+//       <BlogSlider />
+//     </>
+//   );
+// };
+
+// export default page;
+
 import React from "react";
 
-import Hero from "@/components/miniWidgets/Hero";
-import FAQAccordion from "@/components/miniWidgets/faqs";
-import BlogSlider from "@/components/widgets/home/BlogSlider";
 const page = () => {
-  return (
-    <>
-      <Hero />
-      <FAQAccordion />
-      <BlogSlider />
-    </>
-  );
+  return <div>faqs</div>;
 };
 
 export default page;
