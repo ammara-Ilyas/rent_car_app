aaa589c762364601ca52c00f109c860fa007d0f9
ammara-Ilyas <ismarailyas545@gmail.com>
2025-03-08 23:03:46 +0500

    faqs updated

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,24 +1,16 @@
-// import React from "react";
-
-// import Hero from "@/components/miniWidgets/Hero";
-// import FAQAccordion from "@/components/miniWidgets/faqs";
-// import BlogSlider from "@/components/widgets/home/BlogSlider";
-// const page = () => {
-//   return (
-//     <>
-//       <Hero />
-//       <FAQAccordion />
-//       <BlogSlider />
-//     </>
-//   );
-// };
-
-// export default page;
-
 import React from "react";
 
+import Hero from "@/components/miniWidgets/Hero";
+import FAQAccordion from "@/components/miniWidgets/faqs";
+import BlogSlider from "@/components/widgets/home/BlogSlider";
 const page = () => {
-  return <div>faqs</div>;
+  return (
+    <>
+      <Hero />
+      <FAQAccordion />
+      <BlogSlider />
+    </>
+  );
 };
 
 export default page;
