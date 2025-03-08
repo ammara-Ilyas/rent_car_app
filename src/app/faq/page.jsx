445712d522cf8acfa07fb6193974fee2e62fcf35
ammara-Ilyas <ismarailyas545@gmail.com>
2025-03-08 23:34:41 +0500

    updated

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Hero from "@/components/miniWidgets/Hero";
-import FAQAccordion from "@/components/miniWidgets/faqs";
+import FAQAccordion from "@/components/miniWidgets/Faqs";
 import BlogSlider from "@/components/widgets/home/BlogSlider";
 const page = () => {
   return (
